b767585fc6d875b96ac76eeba644b4ef06de2f1b
CrispyCoco <gomezrodriguezj@udesa.edu.ar>
2020-05-22 18:04:47 -0300

    Tracks linkeadas

--- Js/Home.js
+++ Js/Home.js
@@ -1,12 +1,33 @@
 window.onload = function(){
-    fetch('https://api.deezer.com/chart/0')
+    fetch(' https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart/0')
     
     .then(function(response){
 	    return response.json();
     })
     
-    .then(function(information){
-	    console.log(information);
+    .then(function(info){
+        console.log(info);
+        
+        var listTrack = document.querySelector('.list-track');
+        var listArtist = document.querySelector('.list-artist');
+        for (let i = 0; i < info.tracks.data.length; i++) {
+            var element = info.tracks.data[i];
+            var toAdd = '<div class="trackBox">';
+            toAdd += '<div><a href="track.html">' + element.title +'</a></div>';
+            toAdd += '<div class="extraInfo"><a href="artists.html">' + element.artist.name + '</a></div>';
+            toAdd += '<div class="extraInfo"><a href="album.html">' + element.album.title + '</a></div>';
+            toAdd += '</div>';
+            listTrack.innerHTML += toAdd;
+        }
+
+        for (let i = 0; i < info.artists.data.length; i++) {
+            var element = info.artists.data[i];
+            var toAdd = '<div class="trackBox">';
+            toAdd += '<div><a href="track.html">' + element.name +'</a></div>';
+            toAdd += '<div class="extraInfo"><a href="artists.html">' + element.artist.name + '</a></div>';
+            toAdd += '</div>';
+            listArtist.innerHTML += toAdd;
+        }
     })
     
     .catch(function(error){
