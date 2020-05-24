98779bc2b019991a5f3ec92f897b38bd7051c904
CrispyCoco <gomezrodriguezj@udesa.edu.ar>
2020-05-23 21:00:46 -0300

    Final changes home

--- Js/Home.js
+++ Js/Home.js
@@ -18,10 +18,25 @@ window.onload = function(){
             // Seccion top tracks 
             for (let i = 0; i < info.tracks.data.length; i++) {
                 let element = info.tracks.data[i];
+                let name = element.artist.name
+                let title = element.album.title;
+                
+                if(name.length > 16){
+                    console.log(name);
+                    
+                    let namePart = name.slice(0, 19);
+                    name = namePart + '...'
+                    console.log(namePart);
+                    
+                }
+                if (title.length > 16) {
+                    titlePart = element.album.title.slice(0, 15);
+                    title = titlePart + '...'
+                }
                 var toAdd = `<div class="trackBox">
-                                <div><a href="track.html">${element.title}</a></div>
-                                <div class="extraInfo"><a href="artists.html">${element.artist.name}</a></div>
-                                <div class="extraInfo"><a href="album.html">${element.album.title}</a></div>
+                                <div><a href="details.html">${element.title}</a></div>
+                                <div class="extraInfo"><a href="details.html">${name}</a></div>
+                                <div class="extraInfo"><a href="details.html">${title}</a></div>
                             </div>`;
                 listTrack.innerHTML += toAdd;
             }
@@ -38,7 +53,7 @@ window.onload = function(){
                     .then(function(info){
 
                         toAdd = `<div class="trackBox">
-                                    <div><a href="track.html">${info.name}</a></div>
+                                    <div><a href="details.html">${info.name}</a></div>
                                     <div class="extraInfo"><a href="sequel.html">${info.nb_fan} fans</a></div>
                                 </div>`;
                         
@@ -56,7 +71,7 @@ window.onload = function(){
             for (let i = 0; i < info.podcasts.data.length; i++) {
                 var element = info.podcasts.data[i];
                 toAdd = `<div class="trackBox">
-                            <div><a href="track.html">${element.title}</a></div>
+                            <div><a href="details.html">${element.title}</a></div>
                             <div class="fans"><a href="sequel.html">${element.fans} fans</a></div>
                         </div>`;
                 listPodcast.innerHTML += toAdd;
