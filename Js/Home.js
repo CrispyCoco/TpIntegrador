678a669f2b3c87aef6c047708cd2abe78b461f49
CrispyCoco <gomezrodriguezj@udesa.edu.ar>
2020-06-04 19:50:29 -0300

    .

--- Js/Home.js
+++ Js/Home.js
@@ -22,22 +22,27 @@ window.onload = function(){
             for (let i = 0; i < info.tracks.data.length; i++) {
                 let element = info.tracks.data[i];
                 let name = element.artist.name;
-                let title = element.album.title;
+                let albumTitle = element.album.title;
+                let title = element.title;
                 
+                if (title.length > 24) {
+                    let titlePart = title.slice(0,23);
+                    title = titlePart + '...';
+                }
                 if(name.length > 16){
-                    console.log(name);
+                    // console.log(name);
                     
                     let namePart = name.slice(0, 15);
                     name = namePart + '...'                    
                 }
-                if (title.length > 16) {
+                if (albumTitle.length > 16) {
                     titlePart = element.album.title.slice(0, 15);
-                    title = titlePart + '...'
+                    albumTitle = titlePart + '...'
                 }
                 toAdd = `<div class="trackBox">
-                                <div><a href="details.html?trackId=${element.id}" class="ellipsis" speed="160">${element.title}</a></div>
+                                <div><a href="details.html?trackId=${element.id}" class="ellipsis" speed="160">${title}</a></div>
                                 <div class="extraInfo"><a href="details.html?artistId=${element.artist.id}" class="ellipsis" speed="160">${name}</a></div>
-                                <div class="extraInfo"><a href="details.html?albumId=${element.album.id}" class="ellipsis" speed="160">${title}</a></div>
+                                <div class="extraInfo"><a href="details.html?albumId=${element.album.id}" class="ellipsis" speed="160">${albumTitle}</a></div>
                             </div>`;
                 listTrack.innerHTML += toAdd;
             }
@@ -52,9 +57,13 @@ window.onload = function(){
                     })
                 
                     .then(function(info){
-
+                        let name = info.name;
+                        if (name.length > 24) {
+                            let namePart = name.slice(0, 23);
+                            name = namePart + '...';
+                        }
                         toAdd = `<div class="trackBox">
-                                    <div><a href="details.html?artistId=${info.id}">${info.name}</a></div>
+                                    <div><a href="details.html?artistId=${info.id}">${name}</a></div>
                                     <div class="extraInfo"><p class="pFans">${info.nb_fan} fans</p></div>
                                 </div>`;
                         
@@ -70,47 +79,54 @@ window.onload = function(){
             // Seccion top Podcasts
             for (let i = 0; i < info.podcasts.data.length; i++) {
                 var element = info.podcasts.data[i];
+                let title = element.title;
+                if (title.length > 24) {
+                    let titlePart = title.slice(0,23);
+                    title = titlePart + '...';
+                }
+                console.log(title);
+                
                 toAdd = `<div class="trackBox">
-                            <div><a href="details.html?podcastId=${element.id}">${element.title}</a></div>
+                            <div><a href="details.html?podcastId=${element.id}">${title}</a></div>
                             <div class="fans extraInfo"><p class="pFans">${element.fans} fans</p></div>
                         </div>`;
                 listPodcast.innerHTML += toAdd;
             }
 
-                const ellipsisElements = document.getElementsByClassName('ellipsis');
-                console.log(ellipsisElements);
+                // const ellipsisElements = document.getElementsByClassName('ellipsis');
+                // // console.log(ellipsisElements);
                 
-                for (const element of ellipsisElements) {
-                   console.log(element);
+                // for (const element of ellipsisElements) {
+                //    console.log(element);
                    
-                   element.innerHTML = `<span style>${element.innerText}</span>`;
-                   const span = element.getElementsByTagName('span')[0];
+                //    element.innerHTML = `<span style>${element.innerText}</span>`;
+                //    const span = element.getElementsByTagName('span')[0];
                    
-                   element.addEventListener('mouseover', () => {
-                      console.log('#');
-                      const speed = parseInt(element.getAttribute('speed'));
-                      const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
-                      const time = length / speed;
-                      span.style.transition = `left ${time}s linear`;
-                      span.style.left = `-${length}px`;
-                   });
+                //    element.addEventListener('mouseover', () => {
+                //       console.log('#');
+                //       const speed = parseInt(element.getAttribute('speed'));
+                //       const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
+                //       const time = length / speed;
+                //       span.style.transition = `left ${time}s linear`;
+                //       span.style.left = `-${length}px`;
+                //    });
              
-                   element.addEventListener('mouseout', () => {
-                      span.setAttribute('style', '');
-                   });
+                //    element.addEventListener('mouseout', () => {
+                //       span.setAttribute('style', '');
+                //    });
              
-                   element.addEventListener('click', () => {
-                      if (span.getAttribute('style')) {
-                         span.setAttribute('style', '');
-                      } else {
-                         const speed = parseInt(element.getAttribute('speed'));
-                         const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
-                         const time = length / speed;
-                         span.style.transition = `left ${time}s linear`;
-                         span.style.left = `-${length}px`;
-                      }
-                   });
-                }
+                //    element.addEventListener('click', () => {
+                //       if (span.getAttribute('style')) {
+                //          span.setAttribute('style', '');
+                //       } else {
+                //          const speed = parseInt(element.getAttribute('speed'));
+                //          const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
+                //          const time = length / speed;
+                //          span.style.transition = `left ${time}s linear`;
+                //          span.style.left = `-${length}px`;
+                //       }
+                //    });
+                // }
 
             slider.innerHTML += `<li>
                                     <a href="details.html?trackId=${info.tracks.data[0].id}">
