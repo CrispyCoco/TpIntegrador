72fab20d1c89deea87e3613dfd43bcb7d43d7aea
isaacyoon <iyoon@udesa.edu.ar>
2020-06-05 16:20:24 -0300

    cambios en detail.html

--- Js/details.js
+++ Js/details.js
@@ -1,18 +1,19 @@
 window.onload = function(){
 
-    var visualizar = prompt("Que queres ver?");
-    var artist = document.querySelector(".container-artist");
-    var album = document.querySelector(".container-album");
-    var genre = document.querySelector(".container-genre");
-    var track = document.querySelector(".container-track");
+    
+        
+        var artist = document.querySelector(".container-artist");
+        var album = document.querySelector(".container-album");
+        var genre = document.querySelector(".container-genre");
+        var track = document.querySelector(".container-track");
 
-    if (visualizar == "artist") {
-        artist.style.display = "flex";
-    } else if (visualizar == 'album') {
-        album.style.display = 'flex';
-    } else if (visualizar == 'genre') {
-        genre.style.display = 'flex';
-    } else if (visualizar == 'track') {
-        track.style.display = 'flex';
-    }
+        // if (visualizar == "artist") {
+        //     artist.style.display = "flex";
+        // } else if (visualizar == 'album') {
+        //     album.style.display = 'flex';
+        // } else if (visualizar == 'genre') {
+        //     genre.style.display = 'flex';
+        // } else if (visualizar == 'track') {
+        //     track.style.display = 'flex';
+        // }
 }
