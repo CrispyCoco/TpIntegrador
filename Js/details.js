9a7710cfbccf1bcfa9bf15c7e231edd1c392f56a
isaacyoon <iyoon@udesa.edu.ar>
2020-06-10 15:53:51 -0300

    .

--- Js/details.js
+++ Js/details.js
@@ -15,6 +15,30 @@ window.onload = function(){
             // QUERYSELECTOR DE LA CLASE QUE QUIERA MOSTRAR(TRACK)
             var track = document.querySelector(".container-track");
             track.style.display = "flex";
+            toAdd=
+            `<div class="track">
+                <div class="imgContainer">
+                    <div class="overImage">
+                        
+                    </div>
+                    <img src="${info.album.cover_big}" alt="" class="imagen">
+                </div>
+        
+                <div class="info">
+                    <h3 class="title">${info.title}</h3>
+                    <div class="moreInfo">
+                        <a href="artists.html" class="artist">${info.artist.name}</a>
+                        <p class="sideBar">/</p>
+                        <a href="album.html" class="album">${info.album.title}</a>
+                    </div>
+                    
+                    <div class="button">
+                        <a href="sequel.html" class="add">Add to playlist</a>
+                    </div>
+                    <div class="duration"><h5>${info.duration}</h5></div>
+                </div>
+            </div>`
+            track.innerHTML+=toAdd;
         })
         .catch(function(error){
             console.log('el error fue: '+ error);
@@ -34,9 +58,7 @@ window.onload = function(){
             let image = info.picture_big;
             let name = info.name;
             let fans = info.nb_fan;
-            artist.innerHTML = 
-            // '<img alt="'+ name +'" src="'+ image +'">';
-            
+            toAdd = 
                 `<div class="artist">
                     <div class="imgContainer">
                         <img src="${image}" alt="">
@@ -44,23 +66,22 @@ window.onload = function(){
                     <div class="info">
                         <div class="headline">
                             <h3>${name}</h3>
-                            <h4>Populares</h4>
+                            <h4>${fans} Fans</h4>
                         </div>
                         <div class="musica">
                             <h5>Top Tracks</h5>
-                            <ul>
-                                <li><a href="track.html">Circles</a></li>
-                                <li><a href="track.html">Rockstar</a></li>
-                                <li><a href="track.html">Sunflower</a></li>
-                                <li><a href="track.html">Wow.</a></li>
-                                <li><a href="track.html">Goodbyes</a></li>
-                                <li><a href="track.html">Saint-Tropez</a></li>
-                            </ul>
+                            <ol>
+                            <li><a href="track.html">Circles</a></li>
+                            <li><a href="track.html">Rockstar</a></li>
+                            <li><a href="track.html">Sunflower</a></li>
+                            <li><a href="track.html">Wow.</a></li>
+                            <li><a href="track.html">Goodbyes</a></li>
+                            <li id="last"><a href="track.html">Saint-Tropez</a></li>
+                            </ol>
                         </div>
                     </div>
-                
-                 </div>`
-            
+                 </div>`;
+            artist.innerHTML+=toAdd;
             
         })
         .catch(function(error){
@@ -78,6 +99,31 @@ window.onload = function(){
             console.log(info);
             var album= document.querySelector(".container-album");
             album.style.display = "flex";
+            toAdd=
+            `<div class="album">
+                <div class="imgContainer">
+                    <img src="${info.cover_big}" alt="">
+                </div>
+                <div class="info">
+                    <h3>${info.title}</h3>
+                    <div class="moreInfo">
+                        <a href="sequel.html" class="artist">${info.artist.name}</a>
+                        <h5>${info.release_date}- 18 canciones, 1h 4min</h5>
+                    </div>
+                </div>
+            </div>
+            <div class="music">
+                    <h5>Tracks</h5>
+                    <div class="track"><a href="track.html">Paranoid</a></div>
+                    <div class="track"><a href="track.html">Spoil my night</a></div>
+                    <div class="track"><a href="track.html">Rich & Sad</a></div>
+                    <div class="track"><a href="track.html">Zack and Codeine</a></div>
+                    <div class="track"><a href="track.html">Takin' Shots</a></div>
+                    <div class="track"><a href="track.html">Rockstar</a></div>
+                    <div class="track"><a href="track.html">Over Now</a></div>
+                    <div class="track"><a href="track.html">Psycho</a></div>
+            </div>`
+            album.innerHTML+=toAdd;
         })
         .catch(function(error){
             console.log('el error fue: '+ error);
