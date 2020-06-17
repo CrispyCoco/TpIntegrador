51337c95c04b431cc40c676b4404c5d28c6c4697
CrispyCoco <gomezrodriguezj@udesa.edu.ar>
2020-06-17 02:34:32 -0300

    Footer fix

--- js/temp.js
+++ js/temp.js
@@ -1,22 +1,21 @@
 window.onload = function() {
    
-   const ellipsisElements = document.getElementsByClassName('ellipsis');
-   console.log(ellipsisElements);
+const ellipsisElements = document.getElementsByClassName('ellipsis');
+console.log(ellipsisElements);
    
-   for (const element of ellipsisElements) {
-      console.log(element);
+for (const element of ellipsisElements) {
+console.log(element);
+element.innerHTML = `<span style>${element.innerText}</span>`;
+const span = element.getElementsByTagName('span')[0];
       
-      element.innerHTML = `<span style>${element.innerText}</span>`;
-      const span = element.getElementsByTagName('span')[0];
-      
-      element.addEventListener('mouseover', () => {
-         console.log('#');
-         const speed = parseInt(element.getAttribute('speed'));
-         const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
-         const time = length / speed;
-         span.style.transition = `left ${time}s linear`;
-         span.style.left = `-${length}px`;
-      });
+element.addEventListener('mouseover', () => {
+console.log('#');
+const speed = parseInt(element.getAttribute('speed'));
+const length = span.getBoundingClientRect().width - element.getBoundingClientRect().width;
+const time = length / speed;
+span.style.transition = `left ${time}s linear`;
+span.style.left = `-${length}px`;
+});
 
       element.addEventListener('mouseout', () => {
          span.setAttribute('style', '');
